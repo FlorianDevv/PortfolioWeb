79c66e659fe720f28780af5a07a9e4fe41ede56a
FlorianDevv <118461513+FlorianDevv@users.noreply.github.com>
2023-08-17 17:56:25 +0200

    Add Anime.js to the project

--- src/app/views/presentation.tsx
+++ src/app/views/presentation.tsx
@@ -1,8 +1,49 @@
 import Image from "next/image";
 import { useTranslations } from "next-intl";
+import { useEffect, useRef } from "react";
+import anime from "animejs/lib/anime.es.js";
 
 export default function Presentation() {
   const t = useTranslations("Presentation");
+  const titleRef = useRef(null);
+  const subtitleRef = useRef(null);
+  const imageRef = useRef(null);
+  useEffect(() => {
+    anime({
+      targets: titleRef.current,
+      translateX: [-100, 0],
+      opacity: [0, 1],
+      easing: "easeOutElastic(1, .6)",
+      duration: 1000,
+      delay: 100,
+      complete: () => {
+        anime({
+          targets: titleRef.current,
+          scale: [1, 1.1],
+          easing: "easeInOutQuad",
+          duration: 1000,
+          loop: true,
+        });
+      },
+    });
+    anime({
+      targets: subtitleRef.current,
+      translateX: [100, 0],
+      opacity: [0, 1],
+      easing: "easeOutElastic(1, .6)",
+      duration: 1000,
+      delay: 100,
+    });
+    anime({
+      targets: imageRef.current,
+      translateY: [100, 0],
+      opacity: [0, 1],
+      easing: "easeOutExpo",
+      duration: 1500,
+      delay: 100,
+    });
+  }, []);
+
   return (
     <section
       id="Projects"
@@ -15,15 +56,22 @@ export default function Presentation() {
       </header>
       <div className="flex flex-row justify-center items-center flex-wrap">
         <article className="flex flex-col justify-center items-center mt-52 max-w-3xl">
-          <h1 className=" text-7xl lg:text-8xl font-bold text-[#ccf381] drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.3)] max-w-xl text-center ">
+          <h1
+            ref={titleRef}
+            className=" text-7xl lg:text-8xl font-bold text-[#ccf381] drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.3)] max-w-xl text-center "
+          >
             {" "}
             {t("title")}
           </h1>
-          <p className="text-lg m-8 text-white text-center md:text-xl lg:text-2xl">
+          <p
+            ref={subtitleRef}
+            className="text-lg m-8 text-white text-center md:text-xl lg:text-2xl"
+          >
             {t("subtitle")}
           </p>
         </article>
         <Image
+          ref={imageRef}
           src="../images/programming.svg"
           width={500}
           height={500}
